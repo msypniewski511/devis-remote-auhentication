0e208eef693a07f456157f7f4fca8ea1f549c406
msypniewski511 <msypniewski511@gmail.com>
2018-07-26 22:42:18 +0100

    Basic devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.secret_key_base = ENV['SECRET_KEY_BASE']
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: ENV['MAIL_HOST']}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV['MAIL_HOST'],
+    user_name:            ENV["USERNAME"],
+    password:             ENV["PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
