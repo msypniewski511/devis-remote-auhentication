0fb5ad8d3340ed89ea2a24100e3c3e74ea6cbe32
msypniewski511 <msypniewski511@gmail.com>
2018-07-26 22:52:21 +0100

    Added omniauth

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'omniauth'
 
 group :development, :test do
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
